75e0d4ff4c09f6169825f4d92d801089482e45ec
Tuan Anh Le <anhlt1983@gmail.com>
2017-12-04 12:20:51 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace DebugTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer t = new Timer(TimerCallback, null, 0, 2000);
+            Console.ReadLine();
+        }
+
+        static void TimerCallback(Object o)
+        {
+            Console.WriteLine("In TimerCallback: " + DateTime.Now);
+            GC.Collect();
+        }
+    }
+}
